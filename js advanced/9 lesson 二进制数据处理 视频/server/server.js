52a3b7b9af874b43dafe0652b091d74de0d32bfd
wangzichao <zhichao0314@126.com>
2019-07-02 17:03:08 +0800

    video player

---
+++ js advanced/9 lesson 二进制数据处理 视频/server/server.js
@@ -0,0 +1,81 @@
+const Koa = require("koa");
+const Router = require('koa-router');
+const body = require('koa-better-body');
+const koaStatic = require('koa-static');
+const convert = require("koa-convert");
+const fsp = require("fs").promises;
+const path = require("path");
+
+const app = new Koa();
+let router = new Router();
+app.use(convert(body({uploadDir: "./upload"})));
+app.use(router.routes());
+app.use(koaStatic("./static"));
+const SIZE = 5 * 1024 * 1024;
+
+router.use(async (ctx, next) => {
+    //todo check request origin...
+    ctx.set({
+        "Access-Control-Allow-Origin": '*',
+        "Access-Control-Allow-Headers": '*',
+        "Access-ControlAllow-Methods": '*'
+    });
+    await next();
+});
+
+router.post("/upload", async ctx => {
+    console.log(ctx.request.files);
+    ctx.status = 200;
+    ctx.body = "is ok";
+});
+
+router.get("/video/:name", async ctx => {
+
+    const {name} = ctx.params;
+    const {block = 0} = ctx.query;
+    let handle;
+    try {
+        console.log(path.resolve(__dirname, 'video', name));
+        handle = await fsp.open(path.resolve(__dirname, 'video', name), "r");
+        let {buffer, bytesRead} = await handle.read(Buffer.alloc(SIZE), 0, SIZE, SIZE * block);
+        ctx.set({
+            "Content-Type": "video/mp4",
+            "Content-length": bytesRead,
+            "Video-Length": (await handle.stat()).size,
+            "Video-Block": SIZE
+        });
+        ctx.status = 200;
+        ctx.body = buffer;
+    } catch (e) {
+        console.log(e);
+    } finally {
+        if (handle) await handle.close();
+    }
+
+
+});
+
+router.get("/img/:name", async ctx => {
+    const {name} = ctx.params;
+    let handle;
+    try {
+        handle = await fsp.open(path.resolve(__dirname, "img", name), "r");
+        let data = await handle.readFile();
+        ctx.status = 200;
+        ctx.set({
+            "content-type": "image/jpeg",
+            "content-length": handle.stat().size
+        });
+        ctx.body = data;
+    } catch (e) {
+        console.log(e);
+    } finally {
+        if (handle) await handle.close();
+    }
+
+
+});
+
+app.listen(3000, () => {
+    console.log("server in 3000")
+});
