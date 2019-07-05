d9c1e23a71403c6fc33ed956c4e4392c7c5504b4
wangzichao <zhichao0314@126.com>
2019-07-05 17:11:19 +0800

    10

--- js advanced/10 lesson 数据交互封装 1/webpack.config.js
+++ js advanced/10 lesson 数据交互封装 1/webpack.config.js
@@ -2,10 +2,32 @@ const path = require("path");
 module.exports = function (env = {}) {
     const {dev} = env;
     return {
-        mode:dev?"development":"production",
-        entry: path.resolve(__dirname,"src/index.js"),
+        mode: dev ? "development" : "production",
+        entry: path.resolve(__dirname, "src/index.js"),
         output: {
-
+            path: path.resolve(__dirname, "dist"),
+            filename: dev ? "nox.js" : "nox.min.js",
+            libraryTarget: "umd",
+            sourceMapFilename: dev ? "nox.map" : "nox.min.map"
+        },
+        devtool: "source-map",
+        devServer: {
+            port: 8088,
+            open: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/i,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"]
+                        }
+                    }
+                }
+            ]
         }
     }
 };
