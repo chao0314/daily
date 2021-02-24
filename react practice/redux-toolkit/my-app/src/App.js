8a29972e4f4ec9ad40723f6ec50ddc339e840b9e
wangzhichao <zhichao0314@126.com>
2021-02-24 22:24:35 +0800

    'react-toolkit'

---
+++ react practice/redux-toolkit/my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Counter} from './features/counter/Counter';
+import './App.css';
+
+function App() {
+    return (
+
+        <header className="App-header">
+            <Counter/>
+        </header>
+
+    );
+}
+
+export default App;
