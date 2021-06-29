c0066b54600913b0a6d5f6dc7dc8e4bd17b8bff3
wangzichao <zhichao0314@126.com>
2021-06-29 16:42:10 +0800

    'scroll'

---
+++ w-ui/packages/infinite-scroll/index.ts
@@ -0,0 +1,1 @@
+import {WithInstall} from "../../types/types";
---
+++ w-ui/packages/infinite-scroll/lib/WInfiniteScroll.ts
@@ -0,0 +1,13 @@
+import {ObjectDirective} from 'vue';
+
+type InfiniteScrollEl = HTMLElement & {
+    infinite: {
+        container: HTMLElement,
+
+
+    }
+};
+// const WInfiniteScroll:ObjectDirective<ScrollEl> = {
+//
+// }
+
