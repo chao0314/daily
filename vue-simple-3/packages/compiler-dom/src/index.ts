3ab6d2b4030b49afc4aa2270333f4dc66c133d34
wangzichao <zhichao0314@126.com>
2021-07-19 16:40:30 +0800

    'tpl'

--- vue-simple-3/packages/compiler-dom/src/index.ts
+++ vue-simple-3/packages/compiler-dom/src/index.ts
@@ -11,10 +11,106 @@ export const enum NodeTypes {
     VNODE_CALL = 13,
 }
 
+type Context = {
+    line: number,
+    column: number,
+    offset: number,
+    source: string,
+    originalSource: string
+}
+
+type Cursor = {
+    column: number,
+    line: number,
+    offset: number
+}
+
+type Location = {
+    start: Cursor,
+    end: Cursor
+}
+
+type Node = {
+    type: number,
+    content?: string,
+    loc: Location
+
+}
+
+function createParserContext(tpl: string): Context {
+
+    return {
+        line: 1,
+        column: 1,
+        offset: 0,
+        source: tpl,
+        originalSource: tpl
+    }
+
+
+}
+
+function isEnd(context: Context) {
+
+    return context.source.length === 0;
+
+}
+
+function parseText(context: Context) {
+
+    //遇到 < {{ 这两个字符 说明纯文本结束
+    const endTokens = ["<", "{{"];
+    const {source} =  context;
+    let endIndex = 0;
+    for (let i = 0; i < endTokens.length; i++) {
+
+        let index =  source.indexOf(endTokens[i]);
+
+
+    }
+
+
+}
+
+function parseTpl(context: Context) {
+
+    const astNodes: Node[] = [];
+
+    while (!isEnd(context)) {
+        const {source} = context;
+        let node: Node;
+        //模板语法
+        if (source.startsWith("{{")) {
+
+            //元素
+        } else if (source.startsWith("<")) {
+
+
+            //文本
+        } else {
+
+            node = parseText(context);
+
+
+        }
+
+
+    }
+
+}
+
+function baseParse(tpl: string) {
+
+    const context = createParserContext(tpl);
+
+
+}
+
 
 export function baseCompile(template: string) {
     // 讲模板转换成ast语法树
-    console.log(template)
+    const ast = baseParse(template);
+    console.log(ast)
 }
 
 
