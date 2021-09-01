f313adc1688a8cc5e8a03d5cbe516d27cf50c917
wangzichao <zhichao0314@126.com>
2021-09-01 16:41:04 +0800

    s

--- data-structures-algorithm/practice/queue-stack.js
+++ data-structures-algorithm/practice/queue-stack.js
@@ -58,7 +58,6 @@ var MyStack = function () {
  */
 MyStack.prototype.push = function (x) {
 
-    if (!x) return;
     this.queueOne.push(x);
 
 };
@@ -110,5 +109,191 @@ MyStack.prototype.top = function () {
 MyStack.prototype.empty = function () {
 
     return this.queueOne.length === 0;
+
+};
+
+//面试题 03.05. 栈排序 https://leetcode-cn.com/problems/sort-of-stacks-lcci/
+
+var SortedStack = function () {
+
+    this.stack = [];
+
+
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+SortedStack.prototype.push = function (val) {
+
+    const temp = [];
+
+
+    while (this.stack.length > 0) {
+
+        const top = this.peek();
+
+        if (top < val) {
+
+            temp.push(this.stack.pop());
+        } else {
+
+            this.stack.push(val);
+            break;
+        }
+
+
+    }
+    if (this.stack.length === 0) this.stack.push(val);
+
+    while (temp.length > 0) {
+
+        this.stack.push(temp.pop());
+    }
+
+
+};
+
+
+SortedStack.prototype.pop = function () {
+
+    return this.stack.pop();
+
+};
+
+/**
+ * @return {number}
+ */
+SortedStack.prototype.peek = function () {
+
+    // return this.stack.at(-1);
+    return this.stack[this.stack.length - 1] || -1;
+
+};
+
+/**
+ * @return {boolean}
+ */
+SortedStack.prototype.isEmpty = function () {
+    return this.stack.length === 0;
+
+};
+
+//155. 最小栈 https://leetcode-cn.com/problems/min-stack/
+
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function () {
+    this.stack = [];
+    this.cur_min = null;
+
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+
+    if (this.stack.length === 0) this.cur_min = val;
+    else {
+        if (this.cur_min > val) this.cur_min = val;
+    }
+
+
+    this.stack.push({val, cur_min: this.cur_min});
+
+
+};
+
+
+MinStack.prototype.pop = function () {
+
+    const {val} = this.stack.pop();
+    if (this.stack.length > 0) this.cur_min = this.stack[this.stack.length - 1].cur_min;
+    else this.cur_min = null;
+    return val;
+
+
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+
+    return this.stack.length > 0 ? this.stack[this.stack.length - 1].val : null;
+
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+
+    return this.cur_min;
+};
+
+
+//面试题 03.01. 三合一  https://leetcode-cn.com/problems/three-in-one-lcci/
+
+/**
+ * @param {number} stackSize
+ */
+var TripleInOne = function (stackSize) {
+
+    this.stack = [];
+    this.size = stackSize;
+    this.indexs = [0, stackSize, stackSize * 2];
+
+};
+
+/**
+ * @param {number} stackNum
+ * @param {number} value
+ * @return {void}
+ */
+TripleInOne.prototype.push = function (stackNum, value) {
+
+    const index = this.indexs[stackNum]
+    if (index < this.size * (stackNum + 1)) {
+
+        this.stack[index] = value;
+        this.indexs[stackNum]++;
+
+    }
+
+};
+
+/**
+ * @param {number} stackNum
+ * @return {number}
+ */
+TripleInOne.prototype.pop = function (stackNum) {
+
+    const index = this.indexs[stackNum];
+
+    if (index < this.size * stackNum) return -1;
+    const result = this.stack[index];
+    this.stack[index] = null;
+    this.indexs[stackNum]--;
+    return result;
+};
+
+/**
+ * @param {number} stackNum
+ * @return {number}
+ */
+TripleInOne.prototype.peek = function (stackNum) {
+
+};
+
+/**
+ * @param {number} stackNum
+ * @return {boolean}
+ */
+TripleInOne.prototype.isEmpty = function (stackNum) {
 
 };
