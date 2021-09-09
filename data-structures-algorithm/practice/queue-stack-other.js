dadacd6505741ed63175a865205d725bef8a9be3
wangzichao <zhichao0314@126.com>
2021-09-09 16:35:07 +0800

    s

--- data-structures-algorithm/practice/queue-stack-other.js
+++ data-structures-algorithm/practice/queue-stack-other.js
@@ -14,21 +14,73 @@ const largestRectangleArea = function (heights) {
     const RIGHT_GUARD = heights.length;
     const leftIndexes = [];
     const rightIndexes = [];
+    const orderStack = [];
+    let maxArea = 0;
+
 
     for (let i = 0; i < heights.length; i++) {
 
-        if (leftIndexes.length === 0) leftIndexes.push(i);
-        else {
-            const leftMaxVal = heights[leftIndexes[leftIndexes.length - 1]];
-
+        const curH = heights[i];
+        while (orderStack.length > 0) {
+            const leftIndex = orderStack[orderStack.length - 1];
+            const leftMaxH = heights[leftIndex];
+            if (leftMaxH >= curH) {
+                //弹出，栈顶值比当前值大或等于，无影响，后续再找矮墙时，也会被当前值隔开，当前值是更靠近
+                orderStack.pop();
+            } else {
+                //找到最近的  小于柱子的矮墙，记录
+                leftIndexes[i] = leftIndex;
+                break;
+            }
 
         }
+        // 左侧所有的柱子 都不小于当前值,或者当前为初始值
+        if (orderStack.length === 0) leftIndexes[i] = LEFT_GUARD;
+        orderStack.push(i);
 
+    }
+    //清空 单调栈
+    orderStack.length = 0;
+    for (let i = RIGHT_GUARD - 1; i >= 0; i--) {
 
-        const cueH = heights[i];
+        const curH = heights[i];
+        while (orderStack.length > 0) {
+            const rightIndex = orderStack[orderStack.length - 1];
+            const rightMaxH = heights[rightIndex];
+            if (rightMaxH >= curH) {
+                //弹出，栈顶值比当前值大，无影响，后续再找矮墙时，也会被当前值隔开，当前值是更靠近
+                orderStack.pop();
+            } else {
+                //找到第一个矮墙，记录
+                rightIndexes[i] = rightIndex;
+                break;
+            }
 
+        }
+        // 左侧所有的柱子 都不小于当前值，或者当前为初始值
+        if (orderStack.length === 0) rightIndexes[i] = RIGHT_GUARD;
+        orderStack.push(i);
+
+    }
+    // console.log(leftIndexes, rightIndexes)
+
+    for (let j = 0; j < leftIndexes.length; j++) {
+
+        let leftIndex = leftIndexes[j];
+        let rightIndex = rightIndexes[j];
+        const width = rightIndex - leftIndex - 1;
+        const height = heights[j]
+
+        maxArea = Math.max(maxArea, width * height);
 
     }
 
+    return maxArea;
+
 
 };
+
+
+let heights = [1,1];
+
+console.log(largestRectangleArea(heights));
