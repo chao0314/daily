39a18ad6900159b40c166753a79b918ab0965419
wangzhichao <zhichao0314@126.com>
2021-12-22 21:23:24 +0800

    heap

--- data-structures-algorithm/practice/heap.js
+++ data-structures-algorithm/practice/heap.js
@@ -14,12 +14,18 @@ class Heap {
 
     buildHeap(arr) {
 
-        //倒叙 建堆 效率高 O(n)
-        for (let i = arr.length - 1; i >= 0; i--) {
 
-            this.store[this.index++] = arr[i];
-            // 自顶向下 堆化
-            this.topToDownHeapify();
+        arr.unshift(void 0);
+
+        this.store = arr;
+
+        this.index = arr.length;
+
+
+        //倒叙 建堆 效率高 O(n)
+        for (let i = this.store.length; i > 0; i--) {
+
+            this.topToDownHeapify(i);
 
         }
 
@@ -31,9 +37,9 @@ class Heap {
 
         let index = this.index;
 
-        while (this.index > 1) {
+        while (--this.index > 1) {
 
-            this.swap(1, this.index--);
+            this.swap(1, this.index);
 
             this.topToDownHeapify();
 
@@ -59,7 +65,7 @@ class Heap {
     //自底向上 堆化
     downToTopHeapify() {
 
-        let lastIndex = this.index;
+        let lastIndex = this.index - 1;
         let parentIndex = Math.floor(lastIndex / 2);
 
         // 默认是小顶堆
@@ -74,20 +80,22 @@ class Heap {
 
 
     //自顶向下 堆化
-    topToDownHeapify() {
+    topToDownHeapify(parentIndex = 1) {
 
-        let parentIndex = 1;
-        while (parentIndex <= this.index) {
+        while (parentIndex < this.index) {
+
             let index = parentIndex;
             let childIndex = parentIndex * 2;
-            if (childIndex <= this.index && this.compare(this.store[parentIndex], this.store[childIndex]) > 0) index = childIndex;
-            // 两个子节点都大/小  选最后一个
-            if (childIndex + 1 <= this.index && this.compare(this.store[childIndex], this.store[childIndex + 1]) > 0) index = childIndex + 1;
 
+            if (childIndex < this.index && this.compare(this.store[parentIndex], this.store[childIndex]) > 0) {
+                index = childIndex;
+                // 两个子节点都大/小  选最后一个
+                if (childIndex + 1 < this.index && this.compare(this.store[childIndex], this.store[childIndex + 1]) > 0) index = childIndex + 1;
+            }
 
-            console.log("---", parentIndex, index);
 
             // 不需要堆化
+
             if (parentIndex === index) return;
             this.swap(parentIndex, index);
             parentIndex = index;
@@ -98,7 +106,7 @@ class Heap {
     insert(element) {
 
         if (this.count) {
-            if (this.index < this.count - 1) {
+            if (this.index < this.count) {
 
                 this.store[this.index++] = element;
 
@@ -116,7 +124,7 @@ class Heap {
 
         if (this.index >= 1) {
 
-            this.swap(1, this.index--);
+            this.swap(1, --this.index);
 
             //堆化 调整
             this.topToDownHeapify();
@@ -141,7 +149,10 @@ class Heap {
 // 堆排序  原地  不稳定  nlog(n)
 
 //小顶堆
-const hp1 = new Heap((a, b) => a - b);
-hp1.buildHeap([4, 1, 3, 2]);
+const hp1 = new Heap((a, b) => a-b);
+hp1.buildHeap([4, 7, 1, 6, 3, 2, 8, 5]);
 console.log(hp1.store)
-// console.log(hp1.sort());
+// hp1.remove();
+// console.log(hp1.store)
+hp1.sort();
+console.log(hp1.store)
