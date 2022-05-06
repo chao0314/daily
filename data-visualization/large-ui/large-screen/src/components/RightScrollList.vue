415711928c4f46ae0a89235d6cecee1e7d02f801
wangzichao <zhichao0314@126.com>
2022-05-06 18:17:26 +0800

    'scroll'

--- data-visualization/large-ui/large-screen/src/components/RightScrollList.vue
+++ data-visualization/large-ui/large-screen/src/components/RightScrollList.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="right-scroll">
-    <l-scroll-list :header="header" :data="data"></l-scroll-list>
+    <l-scroll-list :header="header" :data="data" :size="size" :loop="true"></l-scroll-list>
   </div>
 
 </template>
 
 <script>
 import screenData from "../../data/screen";
+import {ref} from 'vue';
 
 export default {
   name: "RightScrollList",
@@ -15,19 +16,26 @@ export default {
     const header = [{order: '城市订单量'}, {shop: '店铺数'},
       {rider: '接单骑手人数'}, {newShop: '新店铺数量'},
       {avgOrder: '人均订单量'}];
-    const data = screenData.areaSales;
+    const size = 10;
+    const data = ref(screenData.areaSales);
 
+    // setTimeout(() => {
+    //
+    //   console.log('timeout')
+    //   data.value = screenData.areaSales.slice(0).map(item => ({...item, order: 666}))
+    // }, 5000)
     return {
       header,
-      data
+      data,
+      size
     }
   }
 }
 </script>
 
 <style scoped>
-.right-scroll{
+.right-scroll {
   height: 100%;
-  overflow: hidden;
+  width: 100%;
 }
 </style>
