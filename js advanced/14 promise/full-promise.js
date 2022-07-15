33bf3febb51c0f78333f1c462c680746cef55e5d
wzc <zhichao0314@126.com>
2022-07-15 15:28:20 +0800

    p

--- js advanced/14 promise/full-promise.js
+++ js advanced/14 promise/full-promise.js
@@ -4,15 +4,15 @@ const REJECTED = 'rejected';
 
 class IPromise {
 
-    constructor(exector) {
+    constructor(executor) {
 
-        if (!(exector instanceof Function)) throw new Error('need function');
+        if (!(executor instanceof Function)) throw new Error('need function');
         this.fulfilledCbs = [];
         this.rejectedCbs = [];
         this.value = void 0;
         this.reason = void 0;
         this.status = PENDING;
-        this.resolve = value => {
+        const resolve = value => {
 
             if (this.status === PENDING) {
 
@@ -32,7 +32,7 @@ class IPromise {
 
         };
 
-        this.reject = error => {
+        const reject = error => {
 
             if (this.status === PENDING) {
                 queueMicrotask(() => {
@@ -52,11 +52,11 @@ class IPromise {
 
         try {
 
-            exector(this.resolve, this.reject);
+            executor(resolve, reject);
 
         } catch (e) {
 
-            this.reject(e);
+            reject(e);
 
         }
 
@@ -67,7 +67,9 @@ class IPromise {
         onFulfilled = onFulfilled ?? (value => value);
 
         //兼容 catch,没有捕获 继续抛出
-        onRejected = onRejected ?? (error => throw error);
+        onRejected = onRejected ?? (error => {
+            throw error
+        });
 
         return new IPromise((resolve, reject) => {
 
@@ -85,6 +87,7 @@ class IPromise {
                     // catch 捕获以后 后续 promise 处理时 变为 then，所以此处时 resolve
                     // 如果 onRejected 抛出异常 会在 下面 catch捕获
                     resolve(onRejected(this.reason));
+
                 } catch (e) {
                     reject(e);
                 }
@@ -93,8 +96,16 @@ class IPromise {
                 this.fulfilledCbs.push(value => {
 
                     try {
-
                         resolve(onFulfilled(value));
+
+                        //这里有点欠缺 如果then返回的仍是 promise 那么，后续的状态应该由这个promise来控制
+                        // 依次类推 每次返回值都可能时 promise
+                        // if(value instanceof  IPromise){
+                        //
+                        //     value.then(res=> resolve(onFulfilled(value)))
+                        // }
+                        //
+
 
                     } catch (e) {
 
@@ -103,9 +114,15 @@ class IPromise {
 
                 });
 
-                this.rejectedCbs.push(() => {
+                this.rejectedCbs.push(reason => {
 
-                    console.log(123)
+                    try {
+                        resolve(onRejected(reason));
+                    } catch (e) {
+                        reject(e);
+
+                    }
+
 
                 });
 
@@ -115,5 +132,142 @@ class IPromise {
         })
     }
 
+    catch(onRejected) {
+
+        return this.then(void 0, onRejected);
+
+    }
+
+    finally(onFinally) {
+
+        this.then(onFinally, onFinally);
+
+
+    }
+
+    static resolve(value) {
+
+        return new IPromise(resolve => resolve(value));
+
+    }
+
+    static reject(reason) {
+
+        return new IPromise((resolve, reject) => reject(reason));
+    }
+
+    static all(promises) {
+
+        return new IPromise((resolve, reject) => {
+
+            const results = [];
+            let flag = 0;
+            for (let i = 0; i < promises.length; i++) {
+
+                const promise = promises[i];
+
+                promise.then(value => {
+
+                    results[i] = value;
+                    flag++;
+                    if (flag === promises.length) resolve(results);
+
+                }, reason => {
+
+                    reject(reason);
+
+                })
+            }
+        })
+    }
+
+    static allSettled(promises) {
+
+        return new IPromise((resolve, reject) => {
+
+            const len = promises.length;
+            const results = [];
+            let flag = 0;
+            promises.forEach((promise, index) => {
+
+                promise.then(value => {
+
+                    results[index] = {status: FULFILLED, value};
+                    flag++;
+                    if (flag === len) resolve(results);
+                }, reason => {
+
+                    results[index] = {status: REJECTED, reason};
+                    flag++;
+                    if (flag === len) resolve(results);
+                })
+            })
+
+        })
+
+
+    }
+
+    static race(promises) {
+
+        return new IPromise((resolve, reject) => {
+
+            promises.forEach(promise => {
+
+                promise.then(value => resolve(value), reason => reject(reason));
+
+            })
+
+
+        })
+
+
+    }
+
+    static any(promises) {
+
+        return new IPromise((resolve,reject)=>{
+            const reasons = [];
+            let flag = 0;
+            const len= promises.length;
+            promises.forEach((promise,index)=>{
+
+                promise.then(value=>resolve(value),reason=>{
+
+                    reasons[index] = reason;
+                    flag++;
+                    if(flag === len) reject(new AggregateError(reasons));
+                })
+
+
+            })
+
+        })
+
+
+
+    }
 
 }
+
+
+const p1 = new IPromise((resolve, reject) => {
+
+    resolve('resolve');
+
+})
+
+p1.then(res => {
+
+    console.log(res);
+    return 'then 1 result';
+}).then(res => {
+    console.log(res)
+}).then(res => {
+
+    throw  new Error('this is error');
+}).catch(err => {
+    console.log('catch', err)
+}).finally(res => {
+    console.log('this is finally');
+})
